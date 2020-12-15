c0fa439dd894e494ed6b8fcd812a01294e3ba521
asavage7 <62650275+asavage7@users.noreply.github.com>
2020-12-15 13:02:05 -0500

    Update about.js

--- commands/about.js
+++ commands/about.js
@@ -17,7 +17,8 @@ module.exports = {
         .setFooter("Created by @AlexTheSavage#3718", "https://cdn.discordapp.com/avatars/705779777180663912/30d15189dacaf8ab618b659e21997c85.png?size=512")
         .setDescription("Jukebox is an easy to use music bot that can search and play songs and playlists from YouTube.")
         .addField("**Perks**", "The bot is completely free, and is small so music will be less interrupted than larger bots.")
-        .addField("Credits", "Built on SudhanPlayz's Discord Music Bot tutorial using discord.js.")
+        .addField("**Credits**", "Built on SudhanPlayz's Discord Music Bot tutorial using discord.js.")
+        .addField("**How to Contribute**", "Built on SudhanPlayz's Discord Music Bot tutorial using discord.js.")
 
             message.channel.send(embed)
         
