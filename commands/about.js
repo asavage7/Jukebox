89b4b5f336b6dedf0e35e5dec6aaddff49753010
asavage7 <62650275+asavage7@users.noreply.github.com>
2021-03-31 08:51:47 -0400

    added assets

--- commands/about.js
+++ commands/about.js
@@ -14,7 +14,7 @@ module.exports = {
         .setColor("YELLOW")
         .setTitle("About Jukebox")
         .setThumbnail("https://cdn.discordapp.com/avatars/788064562111971349/bc55b69ad425ec117ad4a3a8fa360bb2.png?size=512")
-        .setFooter("Created by @AlexTheSavage#3718", "AlexTheSavage#3718.avatarURL")
+        .setFooter("Created by @AlexTheSavage#3718", "./assets/pfp.png")
         .setDescription("Jukebox is an easy to use music bot that can search and play songs and playlists from YouTube.")
         .addField("**Perks**", "The bot is completely free, and is small so music will be less interrupted than larger bots.")
         .addField("**How to Contribute**", "While I am not accepting donations for the bot, you can help by voting for it [here](https://www.example.com) or by inviting it to your server (;invite).")
