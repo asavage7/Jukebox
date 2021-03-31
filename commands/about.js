0127bbdd47feac567f86fd907651124032622d65
asavage7 <62650275+asavage7@users.noreply.github.com>
2021-03-31 10:33:42 -0400

    hope this works

--- commands/about.js
+++ commands/about.js
@@ -1,4 +1,4 @@
-const { MessageEmbed } = require('discord.js')
+const { MessageEmbed } = require("discord.js")
 
 module.exports = {
     info: {
@@ -8,18 +8,18 @@ module.exports = {
         aliases: ["info"]
     },
 
-    run: async function(client, message, args){
+    run: async function(client, message){
 
         let embed = new MessageEmbed()
-        .setColor("YELLOW")
-        .setTitle("About Jukebox")
-        .setThumbnail("https://raw.githubusercontent.com/asavage7/Jukebox/main/assets/logo.png")
-        .setFooter("Created by @AlexTheSavage#3718", "https://raw.githubusercontent.com/asavage7/Jukebox/main/assets/pfp.png")
-        .setDescription("Jukebox is an easy to use music bot that can search and play songs and playlists from YouTube.")
-        .addField("**Perks**", "The bot is completely free, and is small so music will be less interrupted than larger bots.")
-        .addField("**How to Contribute**", "While I am not accepting donations for the bot, you can help by voting for it [here](https://www.example.com) or by inviting it to your server (;invite).")
+            .setColor("YELLOW")
+            .setTitle("About Jukebox")
+            .setThumbnail("https://raw.githubusercontent.com/asavage7/Jukebox/main/assets/logo.png")
+            .setFooter("Created by @AlexTheSavage#3718", "https://raw.githubusercontent.com/asavage7/Jukebox/main/assets/pfp.png")
+            .setDescription("Jukebox is an easy to use music bot that can search and play songs and playlists from YouTube.")
+            .addField("**Perks**", "The bot is completely free, and is small so music will be less interrupted than larger bots.")
+            .addField("**How to Contribute**", "While I am not accepting donations for the bot, you can help by voting for it [here](https://www.example.com) or by inviting it to your server (;invite).")
 
-            message.channel.send(embed)
+        message.channel.send(embed)
         
     }
 }
--- commands/volume.js
+++ commands/volume.js
@@ -2,25 +2,25 @@ const { MessageEmbed } = require("discord.js");
 const sendError = require("../util/error");
 
 module.exports = {
-  info: {
-    name: "volume",
-    description: "Changes the current bot volume.",
-    usage: "<volume>",
-    aliases: ["v"],
-  },
+    info: {
+        name: "volume",
+        description: "Changes the current bot volume.",
+        usage: "<volume>",
+        aliases: ["v"],
+    },
 
-  run: async function (client, message, args) {
-    const channel = message.member.voice.channel;
-    if (!channel)return sendError("You need to be in a voice channel for me to play music!", message.channel);
-    const serverQueue = message.client.queue.get(message.guild.id);
-    if (!serverQueue) return sendError("There is nothing playing in this server.", message.channel);
-    if (!args[0])return message.channel.send(`The current volume is: **${serverQueue.volume}**`);
-    serverQueue.volume = args[0]; 
-    serverQueue.connection.dispatcher.setVolumeLogarithmic(args[0] / 5);
-    let xd = new MessageEmbed()
-    .setDescription(`Volume is now **${args[0]}**`)
-    .setTitle("Volume")
-    .setColor("BLUE")
-    return message.channel.send(xd);
-  },
+    run: async function (client, message, args) {
+        const channel = message.member.voice.channel;
+        if (!channel)return sendError("You need to be in a voice channel for me to play music!", message.channel);
+        const serverQueue = message.client.queue.get(message.guild.id);
+        if (!serverQueue) return sendError("There is nothing playing in this server.", message.channel);
+        if (!args[0])return message.channel.send(`The current volume is: **${serverQueue.volume}**`);
+        serverQueue.volume = args[0]; 
+        serverQueue.connection.dispatcher.setVolumeLogarithmic(args[0] / 5);
+        let xd = new MessageEmbed()
+            .setDescription(`Volume is now **${args[0]}**`)
+            .setTitle("Volume")
+            .setColor("BLUE")
+        return message.channel.send(xd);
+    },
 };
